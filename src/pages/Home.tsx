bff089522b8606e54433c3967ebf59e3ea143a1e
heli06 <1125605844@qq.com>
2022-06-28 23:29:56 +0800

    redux配置完成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
+import {connect, ConnectedProps} from 'react-redux';
 import {RootStackNavigation} from '../navigator';
+import {RootState} from '@/models/index';
 
-interface IProps {
+const mapStateToProps = ({home, loading}: RootState) => ({
+  num: home.num,
+  loading: loading.effects['home/asyncAdd']
+});
+
+const connector = connect(mapStateToProps);
+
+type ModelState = ConnectedProps<typeof connector>;
+
+interface IProps extends ModelState {
   navigation: RootStackNavigation;
 }
 
@@ -14,14 +25,38 @@ class Home extends React.Component<IProps> {
     });
   };
 
+  handleAdd = () => {
+    const {dispatch} = this.props;
+    dispatch({
+      type: 'home/add',
+      payload: {
+        num: 10,
+      },
+    });
+  };
+
+  asyncAdd = () => {
+    const {dispatch} = this.props;
+    dispatch({
+      type: 'home/asyncAdd',
+      payload: {
+        num: 2,
+      },
+    });
+  };
+
   render() {
+    const {num, loading} = this.props;
     return (
       <View>
-        <Text>Home</Text>
+        <Text>Home{num}</Text>
+        <Text>{loading ? '正在努力计算中' : ''}</Text>
+        <Button title="加" onPress={this.handleAdd} />
+        <Button title="异步加" onPress={this.asyncAdd} />
         <Button title="跳转到详情页" onPress={this.onPress} />
       </View>
     );
   }
 }
 
-export default Home;
+export default connector(Home);
